2f0315c0ebddfd571c07ce4e42311c2d96b0acf7
i845198 <i845198@sjcm00561934a.dhcp.sjc.sap.corp>
2015-07-09 10:52:17 -0700

    checking testcase properties class.

---
+++ TestNGPOC/src/test/java/com/ariba/ai/test/webdriver/TestCaseProperties.java
@@ -0,0 +1,35 @@
+package com.ariba.ai.test.webdriver;
+
+public class TestCaseProperties {
+	public String platform, browser, version;
+
+	public TestCaseProperties() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+	public String getBrowser() {
+		return browser;
+	}
+
+	public void setBrowser(String browser) {
+		this.browser = browser;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+}
